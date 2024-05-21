03313e62b641032bcd71b0690ce024fce2d0cbbb
Faisal Bahari <fbhr1112@gmail.com>
2024-05-21 15:11:42 +0700

    fix accordion style

--- src/components/FAQ/accordion.tsx
+++ src/components/FAQ/accordion.tsx
@@ -11,12 +11,10 @@ const Accordion = ({
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
+    <div className="shadow-xl rounded-md overflow-hidden">
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className={`p-4 flex justify-between w-full items-center relative duration-300 bg-primary ${
-          isOpen ? "rounded-t-md" : "shadow-xl rounded-md"
-        }`}>
+        className="p-4 flex justify-between w-full items-center relative duration-300 bg-primary">
         <span className="md:text-lg text-white font-medium">{title}</span>
         <span>
           <RiArrowRightSLine
@@ -27,8 +25,8 @@ const Accordion = ({
         </span>
       </button>
       <div
-        className={`grid overflow-hidden duration-300 border-b rounded-b-md border-gray-300 ${
-          isOpen ? "grid-rows-[1fr] shadow-xl" : "grid-rows-[0fr]"
+        className={`grid overflow-hidden duration-300 border-gray-300 ${
+          isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
         }`}>
         <div className="overflow-hidden">
           <div className="p-4">{children}</div>

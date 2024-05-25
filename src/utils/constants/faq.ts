a31a4abb1c0fd3b79c25b62dae0f2c4a2498887d
Faisal Bahari <fbhr1112@gmail.com>
2024-05-25 15:44:03 +0700

    feat: faq content

--- src/utils/constants/faq.ts
+++ src/utils/constants/faq.ts
@@ -4,6 +4,10 @@ export const FAQ_ITEM: IFaQ[] = [
   {
     question: "What is Short.co.id",
     answer:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium in tempore itaque commodi aperiam. Perferendis dolor autem saepe illum dolore deserunt, dignissimos amet debitis tempora quo aut distinctio, fuga ipsam.",
+      "Short.co.id is a URL or link shortening service that helps users turn long, hard-to-remember URLs into shorter, easier-to-remember URLs. This service is very useful for various purposes, such as sharing links on social media, sending links via text messages, or inserting links in places that have character restrictions.",
   },
+  {
+    question: 'Why I Should Join?',
+    answer: 'By joining Short.co.id, you get various benefits that can improve your experience and efficiency in managing links, such as customizing your links and monitoring your link statistics.'
+  }
 ];

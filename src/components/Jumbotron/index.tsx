23b15b133afcc2816adf330cdb267e423c099a5f
Faisal Bahari <fbhr1112@gmail.com>
2024-05-25 14:21:51 +0700

    feat: aos in jumbotron

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -3,10 +3,14 @@ const Jumbotron = () => {
     <section
       id="jumbotron"
       className="h-dvh md:h-[700px] w-full flex flex-col justify-center items-center px-4 gap-10">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold text-center">
+      <h1
+        data-aos="fade-up"
+        className="text-3xl md:text-4xl lg:text-5xl text-white font-bold text-center">
         Simple and Fast URL Shortener!
       </h1>
-      <div className="max-w-2xl w-full bg-white rounded-md shadow-2xl p-4 md:p-8 space-y-4">
+      <div
+        data-aos="fade-up"
+        className="max-w-2xl w-full bg-white rounded-md shadow-2xl p-4 md:p-8 space-y-4">
         <div>
           <form action="" className="space-y-2">
             <div>

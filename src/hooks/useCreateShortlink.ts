b09cb097ec3eb55aa5daab8bf184ff84850b8277
Faisal Bahari <fbhr1112@gmail.com>
2024-05-31 05:02:23 +0700

    fix: props create short hooks

--- src/hooks/useCreateShortlink.ts
+++ src/hooks/useCreateShortlink.ts
@@ -1,9 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { UseMutationOptions, useMutation } from "@tanstack/react-query";
 import { ENDPOINTS } from "../constant/api";
 import { http } from "../lib/http";
 import { ShortParams } from "../types/request/short";
+import { AxiosResponse } from "axios";
+import { ShortResponse } from "../types/response/short";
 
-export const useCreateShortlink = ({ onSuccess, onError }: any) => {
+export const useCreateShortlink = (
+  options: UseMutationOptions<
+    AxiosResponse<ShortResponse>,
+    AxiosResponse,
+    ShortParams
+  >
+) => {
   return useMutation({
     mutationFn: async ({ original_link, short_name }: ShortParams) => {
       return await http.post(ENDPOINTS.SHORT.POST, {
@@ -11,7 +19,6 @@ export const useCreateShortlink = ({ onSuccess, onError }: any) => {
         short_name,
       });
     },
-    onSuccess,
-    onError,
+    ...options,
   });
 };
